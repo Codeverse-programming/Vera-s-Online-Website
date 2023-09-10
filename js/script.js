4e0138a07a88e1abf7e6db4b6a54a542c6317db2
programmingwithVee <basseyvera018@gmail.com>
2023-09-10 23:59:13 +0100

    codar online chokolg

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
-$("#video1").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/D3iEE29ZXRM?list=PL4cUxeGkcC9ivBf_eKCPIAYXWzLlPAm6G");
+$("#video1").click(function () {
+    $("video").attr("src", "./video/video.mp4");
 });
 
-$("#video2").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/kVeOpcw4GWY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video2").click(function () {
+    $("video").attr("src", "./video/video.mp4");
 });
 
-$("#video3").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/9D1x7-2FmTA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video3").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/9D1x7-2FmTA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video4").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/pnhO8UaCgxg?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video4").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/pnhO8UaCgxg?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video5").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/0sSYmRImgRY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video5").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/0sSYmRImgRY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video6").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/NbTrGcz4DW8?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video6").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/NbTrGcz4DW8?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video7").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/0XSDAup85SA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video7").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/0XSDAup85SA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video8").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/4pO-HcG2igk?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video8").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/4pO-HcG2igk?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video9").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/rb1GWqCJid4?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video9").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/rb1GWqCJid4?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
 
-$("#video10").click(function() {
-    $("iframe").attr("src", "https://www.youtube.com/embed/tHjxSVaj_wY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+$("#video10").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/tHjxSVaj_wY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
 });
